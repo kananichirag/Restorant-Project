a5aaf6919206c457644bf49f230784007817506e
chiragkanani <chiragkanani144@gmail.com>
2023-06-08 16:29:18 +0530

    add new 1

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))       
 
 app.use('/v1',IndexRoutes);
-app.use('/',(req,res) => {
+app.get('/',(req,res) => {
     res.send("hello")
 })
 
