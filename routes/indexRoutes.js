a0b4b97c858973190880b11c3d0577f11f4e78f3
chiragkanani <chiragkanani144@gmail.com>
2023-06-08 16:53:50 +0530

    add new 1

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -3,6 +3,9 @@ const IndexRoute = express.Router();
 const RestorantRoutes = require('../routes/restorantRoutes');
 const ImageRoute = require('../routes/imageRoute')
 
+IndexRoute.use('/',(req,res)=>{
+    res.send("V1 is working !!!")
+});
 
 IndexRoute.use('/rs',RestorantRoutes);
 IndexRoute.use('/image',ImageRoute);
